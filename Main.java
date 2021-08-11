6213431e8e20c96b6c8c45a73b535fb7d3f3c704
Jason Comstock <71102533+JMComstock@users.noreply.github.com>
2021-08-11 04:41:03 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] array = {1,3,5,7,9,11,15};
+        System.out.println("Original array is: " + Arrays.toString(array));
+
+        reverse(array);
+
+        System.out.println("Reversed array is: " + Arrays.toString(array));
+    }
+
+    private static void reverse(int[] array) {
+        int maxIndex = array.length-1;
+        int halfLength = array.length/2;
+
+        for(int i=0;i<halfLength;i++) {
+            int temp = array[i];
+            array[i] = array[maxIndex-i];
+            array[maxIndex-i] = temp;
+        }
+    }
+}
